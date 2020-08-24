59e999b330773e2570389448c768e53dd550b897
Robert <robert.holwerda@han.nl>
2020-08-24 13:41:00 +0200

    remove onion-head app

--- onion-head/proj_explosion.js
+++
@@ -1,9 +0,0 @@
-class Explosion extends Phaser.GameObjects.Sprite {
-    constructor(scene, x, y){
-
-        super(scene, x, y, "proj_explosion");
-        scene.add.existing(this);
-        this.play("explode");
-        scene.sound.play("explosion")
-    }
-}
