6f7fa122955cf774f54bc566c2872813158e688d
Peter Schulz <peter.schulz@han.nl>
2022-01-31 20:20:17 +0100

    Update agenda.js

--- agenda.js
+++ agenda.js
@@ -1,6 +1,6 @@
 const agenda = {}
 
-agenda.currentLesson = [1,1]  // [ week, lesson in week ] 1-based 
+agenda.currentLesson = [1,2]  // [ week, lesson in week ] 1-based 
 
 // All strings in this data will be preprocessed by a Markdown converter.
 // So you can use **bold**, _italic_ and [hyperlinks](http://www.example.com).
